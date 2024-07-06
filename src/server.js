68cf8f58cfdee86c338c7f06c6fb532fa41cc029
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2024-07-06 14:11:50 -0300

    Finishing

--- src/server.js
+++ src/server.js
@@ -2,26 +2,29 @@ import http from "node:http";
 
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
-
-// Padrão de importação "CommonJS" -> require
-// Padrão de importação "ESmodules" -> import -> "type": "module" no package.json
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
-  const { url, method } = req;
+  const { method, url } = req;
 
   await json(req, res);
 
   const route = routes.find((route) => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = req.url.match(route.path);
+
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
     return route.handler(req, res);
   }
 
-  res.writeHead(404).end();
+  return res.writeHead(404).end();
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+server.listen(3333);
