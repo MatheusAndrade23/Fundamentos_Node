d784e1a32c2904fa7445a49e9c84274e995df9e4
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2024-07-05 21:24:48 -0300

    Routes

--- src/server.js
+++ src/server.js
@@ -1,39 +1,25 @@
 import http from "node:http";
-import { randomUUID } from "node:crypto";
+
 import { json } from "./middlewares/json.js";
-import { Database } from "./database.js";
+import { routes } from "./routes.js";
 
 // Padrão de importação "CommonJS" -> require
 // Padrão de importação "ESmodules" -> import -> "type": "module" no package.json
-
-const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { url, method } = req;
 
   await json(req, res);
 
-  if (method == "GET" && url == "/users") {
-    const users = database.select("users");
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
 
-    return res.end(JSON.stringify(users));
+  if (route) {
+    return route.handler(req, res);
   }
 
-  if (method == "POST" && url == "/users") {
-    const { name, email } = req.body;
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert("users", user);
-
-    return res.end("Criação de usuários");
-  }
-
-  res.end("Hello, World!");
+  res.writeHead(404).end();
 });
 
 server.listen(3000, () => {
